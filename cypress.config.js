26d2a1a0e62a3a09cf506cf6ca830954ebdd146c
elenarumi <extra.madri@gmail.com>
2024-12-04 10:34:25 +0100

    Tiempos de espera

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
+
       // implement node event listeners here
+    },
+    e2e: {
+      pageLoadTimeout: 120000,  // Aumenta el tiempo a 2 minutos
     },
     reporter: "mochawesome",
     reporterOptions: {
